db8cd1e3cf37909680105f3e19f95b68e711d199
Vladislav Potapov <15252vlad@gmail.com>
2022-10-21 18:36:59 +0600

    Remove vercel icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
 import Todo from '../components/Todo/Todo';
 import { useTodos } from '../components/Todo/todo.query';
 
@@ -28,18 +26,6 @@ const Home: NextPage = () => {
           {data?.map(Todo)}
         </div>
       </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
     </div>
   );
 };
