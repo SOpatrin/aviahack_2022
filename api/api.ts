29953c14f869cc5c9e038b3e57f9f85c4003595e
Vladislav Potapov <15252vlad@gmail.com>
2022-10-21 18:36:59 +0600

    Reduce mock todos count

--- api/api.ts
+++ api/api.ts
@@ -2,7 +2,7 @@ import { Todo } from "../components/Todo/todo.model";
 import { apiBasePath, paths } from "../config"
 
 export async function getTodos(): Promise<Todo[]> {
-  return (await fetch(new URL(`${paths.todos}`, apiBasePath))).json();
+  return (await (await fetch(new URL(`${paths.todos}`, apiBasePath))).json()).slice(0, 5);
 }
 
 export async function getTodoById(id: number): Promise<Todo> {
