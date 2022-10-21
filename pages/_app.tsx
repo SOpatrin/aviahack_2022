8b8c2ef4123270202419e1b60b60c288639bc53f
Vladislav Potapov <15252vlad@gmail.com>
2022-10-21 18:36:59 +0600

    Add react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,18 +2,28 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { CssBaseline, StyledEngineProvider } from '@mui/material';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <StyledEngineProvider injectFirst>
-      <CssBaseline>
-        <Head>
-          <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-          <meta name="viewport" content="initial-scale=1, width=device-width" />
-        </Head>
-        <Component {...pageProps} />
-      </CssBaseline>
-    </StyledEngineProvider>
+    <QueryClientProvider client={queryClient}>
+      <StyledEngineProvider injectFirst>
+        <CssBaseline>
+          <Head>
+            <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+            <meta
+              name="viewport"
+              content="initial-scale=1, width=device-width"
+            />
+          </Head>
+          <Component {...pageProps} />
+          <ReactQueryDevtools />
+        </CssBaseline>
+      </StyledEngineProvider>
+    </QueryClientProvider>
   );
 }
 
