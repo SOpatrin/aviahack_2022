9f6e2959e21ce52a2f09983d606c0b0d8d7df65b
Vladislav Potapov <15252vlad@gmail.com>
2022-10-21 18:36:58 +0600

    Add mui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,19 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { CssBaseline, StyledEngineProvider } from '@mui/material';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Head>
-        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-      </Head>
-      <Component {...pageProps} />
-    </>
+    <StyledEngineProvider injectFirst>
+      <CssBaseline>
+        <Head>
+          <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+        </Head>
+        <Component {...pageProps} />
+      </CssBaseline>
+    </StyledEngineProvider>
   );
 }
 
