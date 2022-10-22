345f519461ba2ba1ac25d262389ad7c6933eb324
Vladislav Potapov <15252vlad@gmail.com>
2022-10-22 22:01:13 +0600

    Add delta

--- api/api.ts
+++ api/api.ts
@@ -2,6 +2,6 @@ import { Series } from "../components/StorageChart/series.model";
 import { apiBasePath, paths } from "../config"
 
 export async function getSeries(): Promise<Series[]> {
-  const series: Series[] = (await (await fetch(new URL(`${paths.series}`, apiBasePath))).json()).series;
+  const series: Series[] = (await (await fetch(new URL(`${paths.series}?delta=50`, apiBasePath))).json()).series;
   return series;
 }
