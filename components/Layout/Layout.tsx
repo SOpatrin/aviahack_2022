bb438a59ac349bde7069d6c0c175f8972f9e5014
Vladislav Potapov <15252vlad@gmail.com>
2022-10-23 18:55:59 +0600

    Add header with logo

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,3 +1,4 @@
+import Mts from '../Icons/Mts';
 import Navbar from '../Navbar/Navbar';
 
 type LayoutProps = {
@@ -6,9 +7,18 @@ type LayoutProps = {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="flex min-h-screen justify-center bg-gray-200 text-gray-700">
-      <Navbar />
-      <main className="flex w-full flex-col items-center p-6">{children}</main>
+    <div className="flex min-h-screen w-full flex-col bg-gray-200 text-gray-700">
+      <header className="flex h-16 items-center bg-white px-6 shadow-sm">
+        <div className="text-red-600">
+          <Mts />
+        </div>
+      </header>
+      <div className="flex w-full">
+        <Navbar />
+        <main className="flex w-full flex-col items-center p-6">
+          {children}
+        </main>
+      </div>
     </div>
   );
 };
