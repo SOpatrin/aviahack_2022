a6c4da9ef6504a3776130dcd4c0ba4dd2d5054b6
Vladislav Potapov <15252vlad@gmail.com>
2022-10-23 12:56:46 +0600

    Add file upload

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -1,9 +1,10 @@
 import type { NextPage } from 'next';
 import { useSeries } from '../components/StorageChart/series.query';
-import CardChart from '../components/ChartCard/ChartCard';
-import colors from 'tailwindcss/colors';
 import { ScaleValues } from '../components/StorageChart/StorageChart';
 import { useState } from 'react';
+import Card from '../components/Card/Card';
+import FileUpload from '../components/FileUpload/FileUpload';
+import Button from '../components/Button/Button';
 
 const Upload: NextPage = () => {
   const [keepingScale, setKeepingScale] = useState<ScaleValues>('daily');
@@ -15,12 +16,20 @@ const Upload: NextPage = () => {
   return (
     <>
       <div className="w-full">
-        <CardChart
-          title="Загрузка данных"
-          data={keepingData}
-          onScaleChange={setKeepingScale}
-          color={colors.red[600]}
-        />
+        <Card title="Загрузка данных">
+          <p className="mt-2 text-slate-500">
+            Для того, чтобы загрузить новые данные для расчёта, вставьте в форму
+            ниже специально подготовленный csv файл
+          </p>
+          <div className="mt-6">
+            <FileUpload />
+          </div>
+          <div className="mt-6">
+            <Button active onClick={() => {}}>
+              Загрузить
+            </Button>
+          </div>
+        </Card>
       </div>
     </>
   );
