0c68f7728b9e54c43c87231045e9bc03216f6c05
Vladislav Potapov <15252vlad@gmail.com>
2022-10-23 14:58:41 +0600

    Remove unused import

--- pages/finances.tsx
+++ pages/finances.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from 'next';
 import { useSeries } from '../components/StorageChart/series.query';
 import CardChart from '../components/ChartCard/ChartCard';
-import colors from 'tailwindcss/colors';
 import { ScaleValues } from '../components/StorageChart/StorageChart';
 import { useState } from 'react';
 
